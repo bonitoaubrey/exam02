b94213c06fb51f43bd8ff5becab277e6bf9e45e0
Bonito Aubrey <baubrey@si-g3.kzn.21-school.ru>
2021-09-02 10:28:31 +0300

    inter

--- inter.c
+++ inter.c
@@ -1,17 +1,16 @@
 #include <unistd.h>
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-	int used[255] = {0};
-	int i = 2, j = 0;
+	int used[255] = {0}, i = 2, j;
 	if (argc == 3)
 	{
-		while(i > 0)
+		while (i > 0)
 		{
 			j = 0;
-			while(argv[i][j])
+			while (argv[i][j])
 			{
-				if(i == 2 && !used[(unsigned char)argv[i][j]])
+				if (i == 2 && !used[(unsigned char)argv[i][j]])
 					used[(unsigned char)argv[i][j]] = 1;
 				else if (i == 1 && used[(unsigned char)argv[i][j]] == 1)
 				{
